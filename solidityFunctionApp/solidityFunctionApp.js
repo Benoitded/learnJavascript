1e18da86f7f88c170d820b31554aef68eceefa0c
Benoit <benoit.de-dieuleveult@expleogroup.com>
2022-06-09 17:48:36 +0200

    Improve selection, add inheriting, autofocus

--- solidityFunctionApp/solidityFunctionApp.js
+++ solidityFunctionApp/solidityFunctionApp.js
@@ -1,19 +1,15 @@
+let inheriting = "";
 let visibility = "";
 let mutability = "";
 
-function functionCopy() {
-    /* Get the text field */
-    var copyText = document.getElementById("answerToCopy");
-
-    console.log("Copy =>" + copyText);
-    /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
+//Copy the output text into the clipboard
+function functionCopy(elementID) {
+    let element = document.getElementById(elementID); //select the element
+    let elementText = element.textContent; //get the text content from the element
+    navigator.clipboard.writeText(elementText); //use the copyText function below
 }
 
+//Get data from parameters and update the output
 function updateInfo() {
     console.log("Go in updateInfo()");
     //Underscore
@@ -29,11 +25,6 @@ function updateInfo() {
     if (param2 != "") parameters += param2;
     if ((param2 != "") && (param3 != "")) parameters += ", ";
     if (param3 != "") parameters += param3;
-    //Visibility
-    if (visibility != "") visibility += " ";
-
-    //Mutability
-    if (mutability != "") mutability += " ";
     //Returns
     returns1 = document.getElementById("returns1").value;
     returns2 = document.getElementById("returns2").value;
@@ -48,10 +39,11 @@ function updateInfo() {
         if (returns3 != "") returns += returns3;
         if (returns != "") returns = " <span style='color: #dd4e01;'>returns</span> (<span style='color: #269000;'>" + returns + "</span>)";
     }
-    answer = "<span style='color: #1f67db;'>function</span> <span style='color: #dd4e01;'>" + underscore + name + "</span> (<span style='color: #269000;'>" + parameters + "</span>) " + visibility + mutability + returns + "{}";
+    answer = "<span style='color: #1f67db;'>function</span> <span style='color: #dd4e01;'>" + underscore + name + "</span> (<span style='color: #269000;'>" + parameters + "</span>) " + inheriting + visibility + mutability + returns + "{}";
     //console.log("++> " + answer);
     $("#answerToCopy").html(answer);
 }
+document.querySelector('input').addEventListener('input', updateInfo);
 
 $(document).ready(function() {
     let id = "nothing";
@@ -70,14 +62,14 @@ $(document).ready(function() {
     let answer = "";
     updateInfo()
 
-    $(".visibility-btn").on("click", function() {
-        visibility = $(this).attr("id");
-        updateInfo();
-    });
 
-    $(".mutability-btn").on("click", function() {
-        mutability = $(this).attr("id");
-        updateInfo();
+    //Highlight button clicked for each row
+    $(".b-btn").on("click", function() {
+        parent = $(this).parent().attr('id'); //Get the ID from the parent
+        eval(parent + ' = $(this).attr("id") + " "'); //Sent the ID of the button to the variable (named the same)
+        $("#" + parent + " > button").removeClass("none"); //Remove all "none" class to all button (from parent ID to child button)
+        $(this).addClass("none"); //Add class to the one button clicked
+        updateInfo(); //Call the function
     });
 
 });
