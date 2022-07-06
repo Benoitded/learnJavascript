f21afda28f459fac0c6d5440009ceb45f94e82cb
Benoit <benoitded82@hotmail.fr>
2022-07-07 00:59:20 +0200

    Updated error image

--- libraryApp/libraryApp.js
+++ libraryApp/libraryApp.js
@@ -1,17 +1,14 @@
-let savedImage;
+let savedImage = 0;
 
 function readURL(input) {
     if (input.files && input.files[0]) {
-        //console.log(input);
         savedImage = input;
         var reader = new FileReader();
         reader.onload = function(e) {
             $('.previewImage').attr('src', e.target.result);
         };
         reader.readAsDataURL(input.files[0]);
-        //console.log("info image"+ input.files[0])
-        //savedImage = input.files[0]
-        //console.log(input.files[0])
+
         $(".divImage").show();
         $(".styleSVG").hide();
     } else {
@@ -31,18 +28,16 @@ $(document).ready(function() {
         let date = document.getElementById("date").value;
         let publishing = document.getElementById("publishing").value;
         let isbn = document.getElementById("isbn").value;
-        let cover = 1;
         let description = document.getElementById("description").value;
         if (!title) alertMessage = "Title missing";
         if (!author) alertMessage += "\r\nAuthor missing";
         if (!date) alertMessage += "\r\nDate missing";
         if (!publishing) alertMessage += "\r\nPublishing house missing";
         if (!isbn) alertMessage += "\r\nISBN number missing";
-        if (!cover) alertMessage += "\r\nCover image missing";
+        if (!savedImage) alertMessage += "\r\nCover image missing";
         if (!description) alertMessage += "\r\nDescription missing";
 
-        //if ((!title || !author || !date || !publishing || !isbn || !cover || !description))
-        if (!(title && author && date && publishing && isbn && cover && description))
+        if (!(title && author && date && publishing && isbn && savedImage && description))
         //if (0)
             alert(alertMessage);
         else {
@@ -59,7 +54,6 @@ $(document).ready(function() {
                 '<div class="save-param-content">' + date + '</div>',
                 '<div class="save-param-content">' + publishing + '</div>',
                 '<div class="save-param-content last-param"><img class="toWrite" src="Assets/Pinocchio.jpg" alt="Picture of the book cover of Pinocchio"></div>');
-            //'<div class="save-param-content last-param"><img src="Assets/Pinocchio.jpg" alt="Picture of the book cover of Pinocchio"></div>');
             i++;
             var reader = new FileReader();
             reader.onload = function(e) {
